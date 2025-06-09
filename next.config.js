93c1ffc0c2c6896b577f76c479aca44a9ff54a53
kenta-tanaka <tb1.kenta@outlook.jp>
2025-06-09 12:12:19 +0900

    feat: Prepare for initial deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    output: 'export', // 静的サイトとして出力する設定
+    images: {
+      unoptimized: true, // GitHub Pagesでは画像最適化が使えないため
+    },
+  };
+  
+  module.exports = nextConfig;
